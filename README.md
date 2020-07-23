2a627395fd4d064574eb51443adf6cf9879280bb
Alexander Jeurissen <alexander@jeurissen.email>
2020-07-23 12:08:54 +0200

    rename

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# eslint_daemon_wrapper
-eslint_daemon wrapper written in rust
+# bin_wrapper
+
+Simple binstub wrapper written in rust.
+
+
+### Features:
+
+- decorates bin stubs with the great logging / debugging utilities from Rust.
+- when RUST_LOG=debug is provided:
+ - print start / end time
+ - print duration of command
+
+### WIP
+
+- Add option to redirect stderr / stdout of the wrapped command to either traces, or dedicated output files
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use structopt::*;
+use std::process::{Command, Stdio, exit};
+use std::time::{Instant};
+use log::{info, warn};
+
+extern crate pretty_env_logger;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    command: String,
+    args: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+  pretty_env_logger::init();
+  let args = Cli::from_args();
+
+  let command = args.command.clone();
+  let command_args  = args.args.clone();
+
+  info!("attempting to run '{0}' with args: '{1}'", command, command_args);
+
+  let start = Instant::now();
+  let status = Command::new(args.command)
+                       .arg(args.args)
+                       .stdout(Stdio::inherit())
+                       .stderr(Stdio::inherit())
+                       .status()
+                       .expect("failed to execute process");
+
+
+  let duration = start.elapsed();
+
+  if status.success() {
+    info!("'{0}' finished after {1:?} with exit code: {2:?}", command, duration, status);
+    exit(0)
+  } else {
+    warn!("'{0}' failed after {1:?} with exit code {2:?}", command, duration, status);
+    exit(1)
+  }
 }
