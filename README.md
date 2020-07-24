eeb15849da9022876e041560358873079465b5b3
Alexander Jeurissen <alexander@jeurissen.email>
2020-07-24 16:07:07 +0200

    Implement redirection

--- README.md
+++ README.md
@@ -5,11 +5,39 @@ Simple binstub wrapper written in rust.
 
 ### Features:
 
-- decorates bin stubs with the great logging / debugging utilities from Rust.
+decorates bin stubs with the great logging / debugging utilities from Rust.
+
+#### Start / End time and duration logging [LOG_LEVEL = debug]
 - when RUST_LOG=debug is provided:
  - print start / end time
  - print duration of command
 
-### WIP
+#### StdOut / StdErr redirection
 
-- Add option to redirect stderr / stdout of the wrapped command to either traces, or dedicated output files
+two modes:
+
+- Proxy => simply pass-through the stdout and stderr (default behaviour)
+- Capture => consume stdout and stderr and print it to logs
+  - prints `stdout` to TRACE log level
+  - prints `stderr` to DEBUG log level
+
+#### Command line options
+
+```
+bin-wrapper 0.1.0
+Explanation of bin-wrapper usage.
+
+USAGE:
+    bin-wrapper [OPTIONS] <command> [args]...
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+        --mode <mode>     [default: Proxy]  [possible values: Capture, Proxy]
+
+ARGS:
+    <command>
+    <args>...
+```
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,86 @@
-use structopt::*;
+use structopt::StructOpt;
+use structopt::clap::arg_enum;
 use std::process::{Command, Stdio, exit};
 use std::time::{Instant};
-use log::{info, warn};
+use std::io::{self, Write, Result};
+use std::env;
+use log::{info, trace, error};
 
 extern crate pretty_env_logger;
 
-#[derive(Debug, StructOpt)]
-struct Cli {
-    command: String,
-    args: String,
+arg_enum! {
+    #[derive(Debug)]
+    enum Mode {
+        Capture,
+        Proxy
+    }
 }
 
-fn main() {
+#[derive(StructOpt, Debug)]
+#[structopt(name = "bin-wrapper", about = "Explanation of bin-wrapper usage.")]
+struct Cli {
+  #[structopt(long, possible_values = &Mode::variants(), case_insensitive = true, default_value = "Proxy")]
+  mode: Mode,
+
+  command: String,
+  args: Vec<String>,
+}
+
+fn main() -> Result<()> {
+  // NOTE: ensure default log level is NONE
+  if env::var("RUST_LOG").is_err() {
+    env::set_var("RUST_LOG", "none")
+  }
+
   pretty_env_logger::init();
+
   let args = Cli::from_args();
 
-  let command = args.command.clone();
-  let command_args  = args.args.clone();
+  trace!("command line options: {:?}", args);
 
-  info!("attempting to run '{0}' with args: '{1}'", command, command_args);
+  let command = args.command;
+  let command_args  = args.args.join(" ");
+
+  info!("attempting to run '{}'", command);
 
   let start = Instant::now();
-  let status = Command::new(args.command)
-                       .arg(args.args)
-                       .stdout(Stdio::inherit())
-                       .stderr(Stdio::inherit())
-                       .status()
-                       .expect("failed to execute process");
 
+  let child = Command::new(&command)
+    .arg(&command_args)
+    .stdin(Stdio::inherit())
+    .stdout(Stdio::piped())
+    .stderr(Stdio::piped())
+    .spawn()?;
+
+  let output = child.wait_with_output()?;
 
   let duration = start.elapsed();
 
-  if status.success() {
-    info!("'{0}' finished after {1:?} with exit code: {2:?}", command, duration, status);
+  match args.mode {
+    Mode::Proxy => {
+      io::stdout().write_all(&output.stdout).expect("cant proxy StdOut");
+      io::stderr().write_all(&output.stderr).expect("cant proxy stdErr");
+    },
+    Mode::Capture => {
+      let raw_std_out = String::from_utf8(output.stdout).unwrap();
+
+      raw_std_out
+        .lines()
+        .for_each(|x| trace!("{}", x));
+
+      let raw_std_err = String::from_utf8(output.stderr).unwrap();
+
+      raw_std_err
+        .lines()
+        .for_each(|x| error!("{}", x));
+    }
+  }
+
+  if output.status.success() {
+    info!("'{0}' finished after {1:?} with exit code: {2:?}", command, duration, output.status);
     exit(0)
   } else {
-    warn!("'{0}' failed after {1:?} with exit code {2:?}", command, duration, status);
+    error!("'{0}' failed after {1:?} with exit code {2:?}", command, duration, output.status);
     exit(1)
   }
 }
