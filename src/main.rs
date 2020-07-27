94e98adfd08d2ae19af34992bbcffed3ab8c6c53
Alexander Jeurissen <alexander@jeurissen.email>
2020-07-27 14:43:24 +0200

    remove slog, keep exit handle changes

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
-extern crate slog;
-extern crate slog_async;
-extern crate slog_term;
-
 use atty::Stream;
-use logger::*;
-use slog::{debug, error, info, o, trace};
+use log::{debug, error, info, trace};
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::process::exit;
-use std::sync::{Arc, Barrier};
 use std::thread;
-use threadpool::ThreadPool;
-
 use std::time::Instant;
 use structopt::clap::arg_enum;
 use structopt::StructOpt;
 use subprocess::{Popen, PopenConfig, Redirection};
 
-mod logger;
+extern crate pretty_env_logger;
 
 arg_enum! {
     #[derive(Debug)]
@@ -50,42 +42,34 @@ struct Cli {
   )]
   resume_if_env: Option<String>,
 
-  #[structopt(
-    short,
-    long,
-    parse(from_occurrences),
-    help = "Control the output verbosity"
-  )]
-  verbose: u64,
-
   command: Vec<String>,
 }
 
-fn process_skip_if_env(skip_if_env: Option<String>, logger: &slog::Logger) {
+fn process_skip_if_env(skip_if_env: Option<String>) {
   match skip_if_env {
     Some(x) => {
       if env::var(x).is_ok() {
-        trace!(logger, "*skip_if_env* present, ending execution");
+        trace!("*skip_if_env* present, ending execution");
         exit(0)
       } else {
-        trace!(logger, "*skip_if_env* not set, resuming execution");
+        trace!("*skip_if_env* not set, resuming execution");
       }
     }
-    None => trace!(logger, "*skip_if_env* not present, resuming execution"),
+    None => trace!("*skip_if_env* not present, resuming execution"),
   }
 }
 
-fn process_resume_if_env(resume_if_env: Option<String>, logger: &slog::Logger) {
+fn process_resume_if_env(resume_if_env: Option<String>) {
   match resume_if_env {
     Some(x) => {
       if env::var(x).is_err() {
-        trace!(logger, "*resume_if_env* not set, ending execution");
+        trace!("*resume_if_env* not set, ending execution");
         exit(0)
       } else {
-        trace!(logger, "*resume_if_env* present, resuming execution");
+        trace!("*resume_if_env* present, resuming execution");
       }
     }
-    None => trace!(logger, "*resume_if_env* not present, resuming execution"),
+    None => trace!("*resume_if_env* not present, resuming execution"),
   }
 }
 
@@ -105,33 +89,34 @@ fn pipe(mode: &Mode) -> Redirection {
 }
 
 // NOTE: create a thread to capture and buffer the output of p.stdout / p.stderr
-enum CaptureDataType {
+enum Logger {
   Stdout,
   Stderr,
 }
-fn capture(file: File, capture_data: CaptureDataType, logger: slog::Logger) {
-  let log = logger.clone();
-  let reader = BufReader::new(file);
+fn capture(file: File, logger: Logger) -> thread::JoinHandle<()> {
+  return thread::spawn(move || {
+    let reader = BufReader::new(file);
 
-  reader.lines().for_each(|line| match capture_data {
-    CaptureDataType::Stdout => info!(log, "{}", line.unwrap()),
-    CaptureDataType::Stderr => error!(log, "{}", line.unwrap()),
+    reader.lines().for_each(|line| match logger {
+      Logger::Stdout => info!("{}", line.unwrap()),
+      Logger::Stderr => error!("{}", line.unwrap()),
+    });
   });
 }
 
 fn main() {
   let start = Instant::now();
   let args = Cli::from_args();
-  let logger = setup_logger(args.verbose);
+  pretty_env_logger::init_custom_env("LOG");
 
-  trace!(logger, "command line options: {:?}", args);
+  trace!("command line options: {:?}", args);
 
-  process_skip_if_env(args.skip_if_env, &logger);
-  process_resume_if_env(args.resume_if_env, &logger);
+  process_skip_if_env(args.skip_if_env);
+  process_resume_if_env(args.resume_if_env);
 
   let command = &args.command.join(" ");
 
-  debug!(logger, "attempting to run '{}'", command);
+  debug!("attempting to run '{}'", command);
 
   let p_start = Instant::now();
 
@@ -145,36 +130,16 @@ fn main() {
   )
   .unwrap();
 
-  // NOTE: instantiate threadpool with two workers
-  let n_workers = 2;
-  let pool = ThreadPool::with_name("runner".into(), n_workers);
-  // NOTE: create a barrier that waits for all jobs plus the starter thread
-  let barrier = Arc::new(Barrier::new(n_workers + 1));
-
+  let mut out_handle: Option<thread::JoinHandle<()>> = None;
   if let Redirection::Pipe = pipe(&args.stdout) {
     let stdout = p.stdout.take().unwrap();
-    let log = logger.new(o!("stdout" => ""));
-    let stdout_barrier = barrier.clone();
-
-    pool.execute(move || {
-      capture(stdout, CaptureDataType::Stdout, log);
-
-      // NOTE:wait for the other threads
-      stdout_barrier.wait();
-    });
+    out_handle = Some(capture(stdout, Logger::Stdout));
   }
 
+  let mut err_handle: Option<thread::JoinHandle<()>> = None;
   if let Redirection::Pipe = pipe(&args.stderr) {
     let stderr = p.stderr.take().unwrap();
-    let log = logger.new(o!("stderr" => ""));
-    let stderr_barrier = barrier.clone();
-
-    pool.execute(move || {
-      capture(stderr, CaptureDataType::Stderr, log);
-
-      // NOTE:wait for the other threads
-      stderr_barrier.wait();
-    });
+    err_handle = Some(capture(stderr, Logger::Stderr));
   };
 
   // NOTE: wait for process to finish
@@ -183,8 +148,15 @@ fn main() {
   // NOTE: process has finished, log execution time
   let p_duration = p_start.elapsed();
 
-  // NOTE: wait for workers in threadpool to finish
-  barrier.wait();
+  match out_handle {
+    Some(v) => v.join().unwrap(),
+    None => trace!("stdout logger thread not present, no need to wait."),
+  }
+
+  match err_handle {
+    Some(v) => v.join().unwrap(),
+    None => trace!("stderr logger thread not present, no need to wait."),
+  }
 
   // NOTE: we are done log total execution time
   let duration = start.elapsed();
